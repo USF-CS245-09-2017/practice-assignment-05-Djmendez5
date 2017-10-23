47e7b4db9f46ab86fd71bb5ba209b7fc26dfe7a7
Djmendez5 <31456061+Djmendez5@users.noreply.github.com>
2017-10-23 15:18:47 -0700

    Add files via upload

--- BSTree.java
+++ BSTree.java
@@ -1,111 +1,143 @@
-import org.w3c.dom.Node;
+//import org.w3c.dom.Node;
 
 public class BSTree {
+	
 	private class BSTNode {
-		public BSTNode(Comparable value) {
-			data = value;
-		}
 		public Comparable data; 
 		public BSTNode left; 
 		public BSTNode right; 
 
-}
-private BSTNode root;
-public boolean find(Comparable value) {
-	return find(value,root);
-}
-public boolean find(Comparable value, BSTNode node) {
-	if(node.data.compareTo(value)==0) {
-		return true;
-	}
-	else if(node.data.compareTo(value)<0) {
-		return find(value,node.left);
-			
+		BSTNode(Comparable newdata) {
+			data = newdata;
 		}
-	else {
-		return find(value, node.right);
+	}
+	
+	private BSTNode root; 
+
+	public void insert(Comparable value) {
+		root = insert(root, value);
+	}
+	public boolean find(Comparable value) {
+		return find(root, value);
+	}
+	public void delete(Comparable value) {
+		root = delete(root, value);
 	}
 
-}
-public BSTNode insert(Comparable value) {
-	return insert(value,root);
-}
-public BSTNode insert( Comparable value, BSTNode node) {
 	
-	if (node == null) {
-		return new BSTNode(value);
+	public void print() {
+		print(root);
 	}
-	if (value.compareTo(root.data) < 0) {
-		root.left = insert(value, root.left);
-		return root;
-	} else {
-		root.right = insert(value,root);
-		return root;
+
+
+	private boolean find(BSTNode node, Comparable value) {
+		if (node == null)
+			return false;
+		if (value.compareTo(node.data) == 0)
+			return true;
+		if (value.compareTo(node.data) < 0)
+			return find(node.left, value);
+		else
+			return find(node.right, value);
 	}
-}
-public BSTNode delete(Comparable value) {
-	return delete(root, value);
-}
-public BSTNode delete(BSTNode node, Comparable value) {
+
+
+	private Comparable minimum(BSTNode node) {
+		if (node == null)
+			return null;
+		if (node.left == null)
+			return node.data;
+		else
+			return minimum(node.left);
+	}
+
 	
-	if (root == null)
-		return null;
-	if (root.data.compareTo(value) == 0) { 
-		if (root.left == null) 
-			return root.right;
-		else if (root.right == null)
-			return root.left;
-		else { 
-			if (root.right.left == null) { 
-				
-				root.data = root.right.data;
-				
-				root.right = root.right.right;
-				return root;
-			} else {
-				
-				root.data = removesmallest(root.right);
-				return root;
+	private void print(BSTNode node) {
+		if (node != null) {
+			print(node.left);
+			System.out.print(" " + node.data + " ");
+			print(node.right);
+		}
+	}
+
+	
+	private BSTNode insert(BSTNode node, Comparable value) {
+		if (node == null) {
+			return new BSTNode(value);
+		}
+		if (value.compareTo(node.data) < 0) {
+			node.left = insert(node.left, value);
+			return node;
+		} else {
+			node.right = insert(node.right, value);
+			return node;
+		}
+	}
+
+	private BSTNode delete(BSTNode node, Comparable value) {
+		if (node == null)
+			return null;
+		if (node.data.compareTo(value) == 0) { 
+			if (node.left == null) 
+				return node.right;
+			else if (node.right == null) 
+				return node.left;
+			else { 
+				if (node.right.left == null) { 
+					node.data = node.right.data;
+					node.right = node.right.right;
+					return node;
+				} else {
+					
+					node.data = removeSmallest(node.right);
+					return node;
+				}
 			}
+		} else if (value.compareTo(node.data) < 0) { 
+			node.left = delete(node.left, value);
+			return node;
+		} else {
+			node.right = delete(node.right, value); 
+			return node;
 		}
-	} else if (value.compareTo(root.data) < 0) { 
-		root.left = delete(root.left, value);
-		return root;
-	} else {
-		root.right = delete(root.right, value); 
-		return root;
 	}
-}
 
-
-private Comparable removesmallest(BSTNode node) {
 	
-	if (root.left.left == null) {
-		Comparable smallest = root.left.data;
-		root.left = root.left.right;
-		return smallest;
-	} else {
-		return removesmallest(root.left);
+	private Comparable removeSmallest(BSTNode node) {
+		if (node.left.left == null) {
+			Comparable smallest = node.left.data;
+			node.left = node.left.right;
+			return smallest;
+		} else {
+			return removeSmallest(node.left);
+		}
 	}
 
-}
-public Object toStringInOrder() {
+
+public String toStringInOrder() {
 	return toStringInOrder(root);
 }
-private Object toStringInOrder(BSTNode tree) {
-	Object order=null;
-	if (tree != null) {
-		toStringInOrder(tree.left);
-		order = order + " " + tree.data;
-		toStringInOrder(tree.right);
+private String toStringInOrder(BSTNode node) {
+	String order="";
+	if (node != null) {
+		toStringInOrder(node.left);
+		order = order + " " + node.data;
+		toStringInOrder(node.right);
 	}
 return order;
 }
 public String toStringPreOrder() {
 	return toStringPreorder(root);
 }
-public String toStringPreorder(BSTNode tree) {
-	return null;
+public String toStringPreorder(BSTNode node) {
+	String order="";
+	if(node != null) {
+		order = order+ " " + node.data;
+		toStringPreorder(node.left);
+		toStringPreorder(node.right);
+	
+	}
+	return order;
 	
 }
 
